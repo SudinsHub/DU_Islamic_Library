b8d0bb4fcec092c458b6a25f1a202c2143510d03
Naimul Islam <naimulislam747@gmail.com>
2025-11-02 18:52:33 +0600

    42th commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,6 +55,12 @@ const Navbar = () => {
         // Implement save changes logic here
         try {
             const id = (userType === 'reader') ? user.reader_id : (userType === 'volunteer') ? user.volunteer_id : user.admin_id;
+            if(!form.name && user.name) form.name = user.name;
+            if(!form.email && user.email) form.email = user.email;
+            if(!form.contact && user.contact) form.contact = user.contact;
+            if(!form.address && user.address) form.address = user.address;
+            if(!form.room_no && user.room_no) form.room_no = user.room_no;
+
             await axios.put( `${baseUrl}/api/user`, {...form, userType, id}, {
                 headers: {
                     Authorization: `Bearer ${token}`
